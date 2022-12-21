c8475fb3776dbf4f71be13cf29c8f3938ac673ab
Yn-Olvr <yannd38@gmail.com>
2022-12-21 11:56:58 -0500

    fixed the security_group.tf file

--- security_group.tf
+++ security_group.tf
@@ -1,26 +1,34 @@
 resource "aws_security_group" "sg" {
-   name = "allow_ssh_http"
-   description = "allow ssh http inbound traffic"
-   vpc_id = aws_vpc.app_vpc.id
+    name = "allow_ssh_http"
+    description = "allow ssh http inbound traffic"
+    vpc_id = aws_vpc.app_vpc.id
 
 
-ingress {
-    description       = "ssh from vpc"
-    from_port         = 22
-    to_port           = 22
-    cidr_blocks       = ["0.0.0.0/0"]
-    ipv6_cidr_blocks  = ["::/0"]
-}
+    ingress {
+        description       = "ssh from vpc"
+        from_port         = 22
+        to_port           = 22
+        cidr_blocks       = ["0.0.0.0/0"]
+        ipv6_cidr_blocks  = ["::/0"]
+    }
+    ingress {
+        description      = "HTTP from VPC"
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
-  tags = {
-    Name = "allow_ssh_http"
-  }
+    tags = {
+        Name = "allow_ssh_http"
+    }
 }
